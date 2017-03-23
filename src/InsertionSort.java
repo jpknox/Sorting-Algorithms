23cc7cd104e55dc2511b17b742ae95ea66578128
Joao Paulo Knox <joaoknox@hotmail.com>
2017-03-23 17:15:12 +0000

    Renamed variable.

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -16,16 +16,16 @@ public class InsertionSort {
 		int[] sorted = arrayGenerator.copyArray(array);
 		int n = sorted.length;
 		for (int j = 1; j < n; j++) {
-			int key = sorted[j];
-			gui.display("Key is ", key);
+			int newValue = sorted[j];
+			gui.display("Key is ", newValue);
 			int i = j-1;
-			while ( (i > -1) && ( sorted [i] > key ) ) {
-				out.printf("%d at %d is bigger than key of %d at %d.\n", sorted[i], i, key, j);
+			while ( (i > -1) && ( sorted [i] > newValue ) ) {
+				out.printf("%d at %d is bigger than key of %d at %d.\n", sorted[i], i, newValue, j);
 				out.printf("Shifting %d in to %d. ", sorted[i], sorted[i+1]);
 				sorted [i+1] = sorted [i];
 				i--;
 			}
-			sorted[i+1] = key;
+			sorted[i+1] = newValue;
 			out.printf("%s\n\n", gui.display(sorted));
 
 		}
